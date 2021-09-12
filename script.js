9a5f406b41d2d004625f5c2fffa76b1c3df884aa
Loctarogar <igrsl1990@gmail.com>
2021-09-12 20:58:07 +0200

    moving left to right without space between images

--- script.js
+++ script.js
@@ -111,6 +111,44 @@ function moveCarouselTo(slide) {
     }
   }
 }
+// function moveCarouselTo(slide) {
+//   console.log("in moveCarouselTo");
+//   // Check if carousel is moving, if not, allow interaction
+//   if (!moving) {
+//     // temporarily disable interactivity
+//     disableInteraction(); // Update the "old" adjacent slides with "new" ones
+//     console.log("after disableInteractions");
+//     var newPrevious = slide - 1,
+//       newNext = slide + 1,
+//       oldPrevious = slide - 2,
+//       oldNext = slide + 2; // Test if carousel has more than three items
+//     console.log(totalItems);
+//     if (totalItems - 1 > 3) {
+//       console.log("if totalItems > 3");
+//       // Checks and updates if the new slides are out of bounds
+//       if (newPrevious <= 0) {
+//         oldPrevious = totalItems - 1;
+//       } else if (newNext >= totalItems - 1) {
+//         oldNext = 0;
+//       } // Checks and updates if slide is at the beginning/end
+//       if (slide === 0) {
+//         newPrevious = totalItems - 1;
+//         oldPrevious = totalItems - 2;
+//         oldNext = slide + 1;
+//       } else if (slide === totalItems - 1) {
+//         newPrevious = slide - 1;
+//         newNext = 0;
+//         oldNext = 1;
+//       } // Now we've worked out where we are and where we're going,
+//       // by adding/removing classes we'll trigger the transitions.      // Reset old next/prev elements to default classes
+//       items[oldPrevious].className = itemClassName;
+//       items[oldNext].className = itemClassName; // Add new classes
+//       items[newPrevious].className = itemClassName + " prev";
+//       items[slide].className = itemClassName + " active";
+//       items[newNext].className = itemClassName + " next";
+//     }
+//   }
+// }
 
 function initCarousel() {
   console.log("classes");
